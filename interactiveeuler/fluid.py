7ad6b06a0121af186f3de0cecb7c0c00ff902e4b
Patrick Eastham <peastham@math.fsu.edu>
2021-02-24 18:26:50 -0500

    addd LinInterp

--- interactiveeuler/fluid.py
+++ interactiveeuler/fluid.py
@@ -1,8 +1,71 @@
-def euler_step(f0, x0, t0, dt):
-    """Euler's method step to solve x' = f(x,t) with x(t0) = x0
-    and f(x0, t0) = f0.
+from scipy.interpolate import RegularGridInterpolator
+import numpy as np
 
-    USAGE:
+# comment out for production code
+import matplotlib.pyplot as plt
+
+
+class Grid():
+    """2D Grid for computational domain.
+
+    This grid is a square [0,1]^2 domain that includes the endpoints.
+
+    Note
+    ----
+    Do not include the `self` parameter in the ``Parameters`` section.
+
+    Parameters
+    ----------
+    N : int
+        Number of nodes along each axis.
+
+    Attributes
+    ----------
+    x : ndarray(dtype=float, ndim=1)
+        1D array of points in x-coordinate direction.
+    y : ndarray(dtype=float, ndim=1)
+        1D array of points in y-coordinate direction.
+    X : ndarray(dtype=float, ndim=2)
+        2D array of points representing x-coordinate; generated from meshgrid.
+    Y : ndarray(dtype=float, ndim=2)
+        2D array of points representing y-coordinate; generated from meshgrid.
+
+    """
+    def __init__(self, N):
+        self.x = np.linspace(0, 1, num=N)
+        self.y = np.linspace(0, 1, num=N)
+        self.X, self.Y = np.meshgrid(self.x, self.y, indexing='ij')
+
+
+class Fluid():
+    def __init__(self, N=100, visc=1.0, kS=1.0, aS=1.0, dt=0.01):
+        # fields
+        self.U0 = Velocity(N)
+        self.U1 = Velocity(N)
+        self.S0 = np.zeros((N, N), dtype=float)
+        self.S1 = np.zeros((N, N), dtype=float)
+
+        # physical parameters
+        self.visc = visc
+        self.kS = kS
+        self.aS = aS
+
+        # numerical parameters
+        self.dt = dt
+
+
+class Velocity():
+    def __init__(self, N):
+        self.U = np.ones((N, N), dtype=float)
+        self.V = np.ones((N, N), dtype=float)
+
+
+def euler_step(f0, x0, t0, dt):
+    """One step of Euler's method for solving ODEs.
+
+    Solves x' = f(x,t) with x(t0) = x0 and f(x0, t0) = f0.
+
+    Example:
         x1 = euler_step(f0, x0, t0, dt)
     """
 
@@ -11,13 +74,12 @@ def euler_step(f0, x0, t0, dt):
     return x1
 
 
-
-
-
-def LinInterp(S, X0):
+def LinInterp(S, X0, grid):
     """ Linearly interpolates value of field S at point X0
     to scalar value S0. Returns S0. """
-    pass
+
+    interp_func = RegularGridInterpolator((grid.x, grid.y), S)
+    return interp_func(X0)
 
 
 def TraceParticle(X, U, dt):
@@ -40,12 +102,12 @@ def addForce(S0, source, dt):
 
     Parameters
     ----------
-    S0 : matrix
+    S0 : ndarray(dtype=float, ndim=2)
         The scalar field, 2d matrix
-    source : matrix
-        The source field, 2d matrix
+    source : ndarray(dtype=float, ndim=2)
+        Source field, 2d matrix
     dt : float
-        time step
+        Time step
 
     Returns
     -------
@@ -54,3 +116,15 @@ def addForce(S0, source, dt):
     """
 
     return S0 + dt*source
+
+
+# These functions are for development only. They should be commented out
+# when code goes to production
+def plot_fluid(grid, solution):
+    X, Y = np.meshgrid(grid.x, grid.y)
+
+    plt.contourf(X, Y, solution.S1, cmap='RdGy')
+    plt.colorbar()
+    plt.quiver(X, Y, solution.U1.U, solution.U1.V)
+    plt.title("Scalar")
+    plt.show()
--- test/test_fluid.py
+++ test/test_fluid.py
@@ -1,15 +1,63 @@
 import unittest
 import numpy as np
-from interactiveeuler.fluid import (LinInterp,
-                                    TraceParticle,
-                                    Transport,
-                                    addForce)
+import interactiveeuler.fluid as fl
+
+
+class ClassesTest(unittest.TestCase):
+
+    def test_Grid(self):
+        N = 150
+        grid = fl.Grid(N)
+        np.testing.assert_allclose(grid.x,
+                                   np.linspace(0, 1, N))
+        np.testing.assert_allclose(grid.y,
+                                   np.linspace(0, 1, N))
+
+    def test_Fluid(self):
+        N = 200
+        visc = 0.5
+        kS = 1.5
+        aS = 5.1
+        dt = 0.01
+        sol = fl.Fluid(N, visc, kS, aS, dt)
+
+        np.testing.assert_almost_equal(sol.visc, visc)
+        np.testing.assert_almost_equal(sol.kS, kS)
+        np.testing.assert_almost_equal(sol.aS, aS)
+        np.testing.assert_almost_equal(sol.dt, dt)
 
 
 class FluidTest(unittest.TestCase):
 
-    def test_LinInterp(self):
-        pass
+    def test_LinInterp_constant(self):
+        N = 5
+        grid = fl.Grid(N)
+
+        xval = 0.3
+        yval = 0.8
+        x0 = np.array([xval, yval])
+
+        # test on constant field
+        constant = 5.2
+        S = constant*np.ones((N, N), dtype=float)
+        value1 = fl.LinInterp(S, x0, grid)
+        np.testing.assert_almost_equal(value1, constant)
+
+        # test on linear field
+        S = grid.X
+        value2 = fl.LinInterp(S, x0, grid)
+        np.testing.assert_almost_equal(value2, xval)
+
+        # test on bilinear field
+        S = grid.X + 2*grid.Y + np.multiply(grid.X, grid.Y)
+        value3 = fl.LinInterp(S, x0, grid)
+        np.testing.assert_almost_equal(value3, xval + 2*yval + xval*yval)
+
+        # test on quadratic field (interpolation not exact)
+        S = np.multiply(grid.X, grid.X)
+        value4 = fl.LinInterp(S, x0, grid)
+        with np.testing.assert_raises(AssertionError):
+            np.testing.assert_almost_equal(value4, xval**2)
 
     def test_TraceParticle(self):
         pass
@@ -23,7 +71,7 @@ class FluidTest(unittest.TestCase):
         dt = 0.3
         source = 5.6*np.ones((N, N), dtype=float)
 
-        np.testing.assert_allclose(addForce(S0, source, dt),
+        np.testing.assert_allclose(fl.addForce(S0, source, dt),
                                    S0 + dt*source)
 
 
